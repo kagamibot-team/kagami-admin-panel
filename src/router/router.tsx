ca0e2efd42c4114b9dc49bb6d1e2c0652d125b4c
Passthem <Passthem@163.com>
2024-09-13 00:19:05 +0800

    移动 App.tsx 到外面

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Index from "../pages/IndexPage";
-import App from "../pages/App";
+import App from "../App";
 import ErrorPage from "../pages/error/ErrorPage";
 
 export const router = createBrowserRouter([

1355993aeb0b7f1781b35c9913fb4847b31c85bb
Passthem <Passthem@163.com>
2024-09-17 00:15:00 +0800

    完成接入

--- src/pages/LogPage.tsx
+++ src/pages/LogPage.tsx
@@ -1,44 +1,15 @@
-import { Space, message, Typography, theme, Alert } from "antd";
-
-import api from "../common/api";
+import { message, Typography, theme } from "antd";
 
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { a11yDark, a11yLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 import DarkValue from '../common/darkvalue'
-
-const logText = `2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:50 [INFO] 群 114514114 群成员信息刷新了一次
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-30 22:14:03 [INFO] Matcher(type='', module=src.base.event.event_root, lineno=36) running complete
-2024-08-31 16:46:23 [ERROR] Running Matcher(type='', module=src.base.event.event_root, lineno=36) failed.
-`
+import React, { PropsWithChildren, useEffect, useState } from "react";
 
 const { Title } = Typography;
 
-const LogBox = ({ children }) => {
-    const { token: { colorBgLayout, borderRadiusLG } } = theme.useToken();
+const LogBox: React.FC<PropsWithChildren> = ({ children }) => {
+    const { token: { colorBgLayout } } = theme.useToken();
     return <div style={{
         height: 500,
         width: '100%',
@@ -51,40 +22,49 @@ const LogBox = ({ children }) => {
 
 export default function Index() {
     const [messageApi, contextHolder] = message.useMessage();
-
     const { token: { colorBgElevated } } = theme.useToken();
+    const [logs, setLogs] = useState("");
+
+    useEffect(() => {
+        const eventSource = new EventSource("/kagami-api/logs");
+
+        eventSource.onmessage = event => {
+            setLogs(prev => prev + "\n" + event.data);
+        };
+
+        eventSource.onerror = err => {
+            messageApi.error("连接到日志服务器时出现问题，请查看控制台，或刷新页面");
+            console.error(err);
+            eventSource.close();
+        }
+
+        return () => {
+            eventSource.close();
+        };
+    }, []);
 
     return <>
         {contextHolder}
         <Title level={2}>日志查询</Title>
-        <Alert
-            message="接口未完全实现"
-            description="目前显示的并非真实日志，待 SSE 接入。"
-            style={{
-                marginBottom: 12
-            }}
-            type="info"
-            showIcon
-        />
         <LogBox>
             <DarkValue.Consumer>
-                { value => <SyntaxHighlighter
+                {value => <SyntaxHighlighter
                     PreTag="div"
                     CodeTag="span"
                     lineNumberStyle={{
-                            background: colorBgElevated,
-                            marginRight: '0.5em'
-                        }}
+                        background: colorBgElevated,
+                        marginRight: '0.5em'
+                    }}
                     customStyle={{
-                        overflowX: 'none',
+                        overflowX: 'unset',
                         padding: 'none',
                         background: 'none',
                         fontFamily: "Consolas",
                     }}
                     language="accesslog"
                     showLineNumbers={true}
-                    style={ value ? a11yDark : a11yLight}>
-                    { logText }
+                    style={value ? a11yDark : a11yLight}>
+                    {logs}
                 </SyntaxHighlighter>}
             </DarkValue.Consumer>
         </LogBox>
